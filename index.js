40d8bb27bc725b7eb7118dbd0a37a94697ad55b1
ygupta <156329651+yagupta77@users.noreply.github.com>
2024-03-23 06:58:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+const quiz = document.querySelector(".quiz")
+console.log("quiz")
+(".quiz", 5, {
+    delay: 1.5,
+    scrambleText: {
+      text: "QUIZ",
+      rightToLeft: true,
+    },
+  });
---
+++ script.js
@@ -0,0 +1,47 @@
+const container = document.querySelector(".container")
+const button = document.querySelector("#start-btn")
+button.addEventListener('click',()=>{
+ container.classList.add("start-btn")
+
+})
+console.log(container)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+/* ScrollTrigger Docs
+
+https://greensock.com/docs/v3/Plugins/ScrollTrigger
+
+*/
+
+
+
+
+
+/* 
+
+learn more GreenSock and ScrollTrigger
+
+https://www.creativeCodingClub.com
+
+new lessons weekly
+less than $1 per week
+
+*/
+  
